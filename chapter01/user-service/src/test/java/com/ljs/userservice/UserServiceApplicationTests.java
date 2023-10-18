cc0ebb5ba2ae1b4495db01e0fa54746df77a13f8
leejinseop <jjjp9966@gmail.com>
2023-10-18 23:32:20 +0900

    add chapter01

---
+++ chapter01/user-service/src/test/java/com/ljs/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljs.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
